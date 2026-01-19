74d278302603d10727137c79a43d7dd75fe197c2
BarisErdal <bariserdal.bme@gmail.com>
2026-01-19 18:15:42 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <p  className='text-amber-200 text-7xl font-bold'>
+        App component
+      </p>
+    </>
+  )
+}
+
+export default App
